e6fb12f9e96a76fab75e61dec55d2f249059c3bc
shamim1845 <samimraj1845@gmail.com>
2025-09-17 22:53:02 +0600

    Implement ESLin and Prettier

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import * as dotenv from "dotenv";
+import express from 'express';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to acquisitions!" });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to acquisitions!' });
 });
 
 export default app;
