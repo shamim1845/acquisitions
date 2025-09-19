e51c0e5a1cd5ff759cca45590ee6ff75ebbd3f57
shamim1845 <samimraj1845@gmail.com>
2025-09-19 11:19:43 +0600

    feat: enhance app security and logging
    - Added helmet middleware for security enhancements.
    - Integrated morgan for HTTP request logging.
    - Included cookie-parser for handling cookies.
    - Implemented a custom logger using winston for structured logging.
    - Updated root endpoint to log access information.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
+import logger from '#config/logger.js';
 
 const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Root endpoint accessed');
+  // logger.error('This is a sample error log');
+
   res.status(200).json({ message: 'Welcome to acquisitions!' });
 });
 
